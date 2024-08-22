df95e068ebd7d8870cdda6be9772dff756119473
Лісовий Віктор <victor.lissoviy.93@gmail.com>
2024-08-22 20:18:11 +0300

    add Test

---
+++ src/test/java/org/home/lissoviy/lecture6/repositories/BookRepositoryTest.java
@@ -0,0 +1,30 @@
+package org.home.lissoviy.lecture6.repositories;
+
+import org.home.lissoviy.lecture6.models.Book;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@DataJpaTest
+class BookRepositoryTest {
+
+  @Autowired
+  private BookRepository bookRepository;
+
+  @Test
+  void findAll() {
+    Book book1 = new Book(1L, "Title 1", "Author 1", 123.0);
+
+    Book savedBook = bookRepository.save(book1);
+
+    Optional<Book> optionalBook1 = bookRepository.findById(savedBook.getId());
+
+    assertTrue(optionalBook1.isPresent());
+  }
+}
